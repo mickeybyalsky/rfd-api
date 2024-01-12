3e030cb64ed41647406eef33401f0817970eb729
mickeybyalsky <mickeybyalsky@gmail.com>
2024-01-12 12:30:20 -0500

    updated docs

--- main.py
+++ main.py
@@ -5,34 +5,38 @@ from database import comment_collection, post_collection, user_collection
 from mangum import Mangum
 
 description = """
-A homage to the genuine RedFlagDeals experience, made with love and inspired by countless hours spent on the real site. 
+A recreation to the RedFlagDeals experience, made for fun and to explore the world of REST API's.
 <br>
 Inspired by a few dabs at the real RFD by me and my friends, such as "How do I downvote a comment?", I said I can make it happen.
 <br>
-I've spent loads of hours on the site in the past year and scored some awsome deals myself.
+I've spent loads of hours on the site in the past year and scored some awsome deals myself. I wanted to learn about REST API's and wondered what better way to do it by trying to recreate a site I loved.
 <br>
 <br>
-This RESTful API serves to recreate a fully functional backend of RedFlagDeals, by having users, posts, and comments and all the interaction that comes along with it, on a mock MongoDB database. 
+This RESTful API serves to recreate a fully functional backend of RedFlagDeals, by having users, posts, and comments and all the interaction that comes along with it, on a MongoDB mock database. 
 <br>
-Have some fun by creating a user, creating a post or reading some posts and maybe leave a comment on an exisiting post!
+Have some fun by creating a user, then creating a post or reading some posts, and maybe leave a comment on an exisiting post!
 <br>
 <br>
 **Steps**: To use the endpoints, expand the dropdown of an endpoint, press "Try it out", fill in any fields if applicable and hit execute!
 <br>
 <br>
+**Using Authorized Endpoints:**: You will first need to register a user, then use the authorize button to log into that user. Then you are all set to use all the endpoints!
 <br>
-Made with Python, FastAPI and MongoDB. Hosted on AWS
-## Users
+<br>
+<br>
+Made with Python, FastAPI and MongoDB. Hosted on AWS EC2
+## 👫 Users 👫
 
 You will be able to:
 
 * Create/Register a user
 * Read all users
+* Read your user
 * Read a user
 * Update a users
 * Delete a user
 
-## Posts
+## 🛒 Posts 🛒
 
 You will be able to:
 
@@ -44,7 +48,7 @@ You will be able to:
 * Upvote a post
 * Downvote a post
 
-## Comments
+## 💬 Comments 💬
 
 You will be able to:
 
@@ -55,6 +59,19 @@ You will be able to:
 * Delete a comment
 * Upvote a comment
 * Downvote a comment
+
+## 🔍 Misc 🔍
+
+You will be able to: 
+
+* Read API statistics
+
+## 🛠️ Roadmap 🛠️
+
+* Administrator/Moderator functions (Delete posts/comments of other users and ban/temporarily disabling users)
+* Enumerate merchants when creating or searching for posts for a more cohesive experience
+* Adding discussion forums
+* Better experience for replying to comments
 """
 
 app = FastAPI(
--- schemas.py
+++ schemas.py
@@ -2,18 +2,6 @@ from models import UserOut
 
 
 def individual_serial_user(user):
-    # return UserOut(id=str(user["_id"]),
-    #                user_display_name=user.get("user_display_name", ""),
-    #                user_email=user.get("user_email", ""),
-    #                user_full_name=user.get("user_full_name", ""),
-    #                user_location=user.get("user_location", ""),
-    #                user_reputation=user.get("user_reputation", 0),
-    #                user_post_count=user.get("user_post_count", 0),
-    #                user_comment_count=user.get("user_comment_count", 0),
-    #                user_join_date=user.get("user_join_date", ""),
-    #                user_role=user.get("user_role", "")).dict()
-
-    
     return {
         "username": user.get("username", ""),
         "user_email": user.get("user_email", ""),
@@ -26,35 +14,30 @@ def individual_serial_user(user):
         "user_role": user.get("user_role", "")
     }
     
-
 def list_serial_user(users):
     return {str(user["_id"]): individual_serial_user(user) for user in users}
 
-
-
 def individual_serial_post(post):
     return {
-        # "id": str(post["_id"]),
-        "post_title": post.get("post_title",""),
-        "post_description": post.get("post_description",""),
-        "post_product_category": post.get("post_product_category",""),
-        "post_link_to_deal": post.get("post_link_to_deal",""),
-        "post_deal_expiry": post.get("post_deal_expiry", ""),
-        "post_sale_price": post.get("post_sale_price", ""),
-        "post_product_discount": post.get("post_product_discount", ""),
-        "post_votes": post.get("post_votes", 0),
-        "post_timestamp": post.get("post_timestamp", ""),
-        "post_author": post.get("post_author", ""),
-        "post_views": post.get("post_views", 0)
+            "post_title": post.get("post_title", ""),
+            "post_description": post.get("post_description", ""),
+            "post_product_category": post.get("post_product_category", ""),
+            "post_link_to_deal": post.get("post_link_to_deal", ""),
+            "post_deal_expiry": post.get("post_deal_expiry", ""),
+            "post_sale_price": post.get("post_sale_price", ""),
+            "post_product_discount": post.get("post_product_discount", ""),
+            "post_votes": post.get("post_votes", 0),
+            "post_timestamp": post.get("post_timestamp", ""),
+            "post_author": post.get("post_author", ""),
+            "post_views": post.get("post_views", 0)
     }
 
 def list_serial_post(posts) -> list:
-    return[individual_serial_post(post) for post in posts]
+    return {str(post["_id"]): individual_serial_post(post) for post in posts}
 
 def individual_serial_comment(comment):
     return {
-        "id":str(comment.get("_id")),
-        "post_id": comment.get("comment_post_id", ""),
+        # "post_id": comment.get("comment_post_id", ""),
         "comment_author": comment.get("comment_author", ""),
         "comment_body": comment.get("comment_body"),
         "comment_votes": comment.get("comment_votes", None),
@@ -62,4 +45,4 @@ def individual_serial_comment(comment):
     }
 
 def list_serial_comment(comments) -> list:
-    return[individual_serial_comment(comment) for comment in comments]
+    return {str(comment["_id"]): individual_serial_comment(comment) for comment in comments}
